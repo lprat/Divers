2a26084f54e4ec59f188442821cd9b9325cf95b1
Lionel <lionel.prat9@gmail.com>
2020-11-19 16:51:05 +0100

    Update test_yara_rules.py

--- Scripts_divers/test_yara_rules.py
+++ Scripts_divers/test_yara_rules.py
@@ -2,6 +2,9 @@ import os
 import yara
 import sys
 pfile={}
+stra=False
+if len(sys.argv) == 3:
+    stra = True
 if os.path.isfile(sys.argv[1]):
     pfile[str(os.path.basename(sys.argv[1]))] = str(sys.argv[1])
 rules = yara.compile(filepaths=pfile, externals={})
@@ -13,7 +16,10 @@ for root, directories, filenames in os.walk(sys.argv[2]):
             continue
         for match in ret_yara:
             r=[]
+            s=[]
             for st in match.strings:
                 if not st[1] in r:
                     r.append(st[1])
-            print('YARA on '+os.path.basename(filename)+' with rules: '+match.rule+' match DEBUG:'+str(r))
+                if not st[2] in s:
+                    r.append(st[2])
+            print('YARA on '+os.path.basename(filename)+' with rules: '+match.rule+' match DEBUG:'+str(r) + 'Str finded:' + str(s))
