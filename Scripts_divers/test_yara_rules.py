12301545ae4015f9d63cf0c75d0b42a2a9609c85
Lionel <lionel.prat9@gmail.com>
2020-11-19 17:25:37 +0100

    Update test_yara_rules.py

--- Scripts_divers/test_yara_rules.py
+++ Scripts_divers/test_yara_rules.py
@@ -8,6 +8,19 @@ if len(sys.argv) == 4:
 if os.path.isfile(sys.argv[1]):
     pfile[str(os.path.basename(sys.argv[1]))] = str(sys.argv[1])
 rules = yara.compile(filepaths=pfile, externals={})
+if os.path.isfile(sys.argv[2]):
+    ret_yara = rules.match(sys.argv[2], externals={}, timeout=120)
+    filename=os.path.basename(sys.argv[2])
+    for match in ret_yara:
+        r=[]
+        s=[]
+        for st in match.strings:
+            if not st[1] in r:
+                r.append(st[1])
+            if stra and not st[2] in s:
+                r.append(st[2])
+        print('YARA on '+os.path.basename(filename)+' with rules: '+match.rule+' match DEBUG:'+str(r) + 'Str finded:' + str(s))
+    sys.exit
 for root, directories, filenames in os.walk(sys.argv[2]):
     for filename in filenames:
         try:
